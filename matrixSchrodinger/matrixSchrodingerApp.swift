1c62b62c27a2ee1f1996daf5ffa8540f1f3713da
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-02-17 14:31:08 -0600

    Initial Commit

---
+++ matrixSchrodinger/matrixSchrodingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  matrixSchrodingerApp.swift
+//  matrixSchrodinger
+//
+//  Created by IIT PHYS 440 on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct matrixSchrodingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
